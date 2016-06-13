57279705d6628da1c5caef864c4f124996b38258
keqi90 <keqi90@163.com>
2016-06-13 17:07:42 +0800

    Initial Commit

---
+++ KQFileDownload/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KQFileDownload
+//
+//  Created by keqi on 16/6/13.
+//  Copyright © 2016年 keqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KQFileDownload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KQFileDownload
+//
+//  Created by keqi on 16/6/13.
+//  Copyright © 2016年 keqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
